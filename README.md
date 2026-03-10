f76b281b70d790d81653468e188267172cf6b635
Kouki Hagiwara <kouki-abc-1108@outlook.jp>
2026-03-10 11:21:13 +0900

    実機とシミュレーション使い分けれるように変更

--- README.md
+++ README.md
@@ -6,5 +6,19 @@
 $ ros2 launch vla_manipulator_moveit_config demo.launch.py
 
 ```
+- デジタルツインさせるコマンド(new)
+```
+$ sudo chmod 666 /dev/serial/by-id/usb-1a86_USB_Single_Serial_5AF7133194-if00
 
+```
+```
+$ ros2 launch vla_manipulator_moveit_config demo.launch.py use_sim:=false
+
+```
+
+- pcとシミュレーションだけで確認したい場合
+```
+$ ros2 launch vla_manipulator_moveit_config demo.launch.py use_sim:=true
+
+```
 
--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -1,7 +1,26 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("so101_new_calib", package_name="vla_manipulator_moveit_config").to_moveit_configs()
-    return generate_demo_launch(moveit_config)
+    # 1. 引数の設定
+    use_sim = LaunchConfiguration("use_sim")
+    
+    # 2. XACROに引数を渡すようにビルド
+    moveit_config = (
+        MoveItConfigsBuilder("so101_new_calib", package_name="vla_manipulator_moveit_config")
+        .robot_description(mappings={"use_sim": use_sim}) # ここでスイッチを渡す
+        .to_moveit_configs()
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim",
+            default_value="true",
+            description="Trueならシミュレーション、Falseなら実機を動かします"
+        ),
+        generate_demo_launch(moveit_config),
+    ])
